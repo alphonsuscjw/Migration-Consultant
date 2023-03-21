35d04ee0e0f22e04e425bf665dfc1fc528e0f92b
alphonsuscjw <chanjiawei1995@gmail.com>
2023-03-21 19:59:51 +0800

    updated graphs

--- static/js/app_dashboard.js
+++ static/js/app_dashboard.js
@@ -90,7 +90,7 @@ function appendOptions () {
 function optionChanged(selectedOption) {
 
     //Call the drawBarChart function to draw the horizontal bar chart for the selected option
-    drawBarChart(selectedOption, data);
+    drawScatterPlot(selectedOption, data);
 
     // Call the drawBubbleChart function to draw the bubble chart for the selected option
     drawRadarChart(selectedOption, data);
@@ -102,49 +102,43 @@ function init() {
     
     let defaultRegion = "Europe";
 
-    //Call the drawBarChart function to draw the horizontal bar chart for the default ID
-    drawBarChart(defaultRegion, data);
+    //Call the drawScatterPlot function to draw the scatter plot for the default ID
+    drawScatterPlot(defaultRegion, data);
 
     //Call the drawBubbleChart function to draw the bubble chart for the default ID
     drawRadarChart(defaultRegion, data);
 
 }
 
-// Function to draw the horizontal bar chart
-function drawBarChart(region, data_js) {
-
+// Function to draw the scatter plot
+function drawScatterPlot(region, data_js) {
+    
     // Filter the data samples to get the sample data for the input ID
     let inputRegion = data_js.filter(country => country.region === region);
 
-    // Sort the inputSample by sample_values in the descending order
-    inputRegion.sort((a, b) => b["Human Development Index (HDI)"] - a["Human Development Index (HDI)"]);
-
-    // Slice the top 10 values and reverse the array to accommodate Plotly's defaults
-    slicedReversedRegion = inputRegion.slice(0, 10).reverse();
+    inputRegion.sort((a, b) => a.Countries.localeCompare(b.Countries));
 
     let country_names = [];
     let HDIs=[];
-    for (i=0; i<slicedReversedRegion.length; i++){
-        country_names.push(slicedReversedRegion[i].Countries);
-        HDIs.push(slicedReversedRegion[i]["Human Development Index (HDI)"]);
+    for (let i=0; i<inputRegion.length; i++){
+        country_names.push(inputRegion[i].Countries);
+        HDIs.push(inputRegion[i]["Human Development Index (HDI)"]);
     }
 
     let trace = {
-        x: HDIs,
-        y: country_names,
-        type: "bar",
-        orientation: "h",
-        title: "Top 10 Countries According to HDI"
+        x: country_names,
+        y: HDIs,
+        type: "scatter",
+        mode: "markers"
     };
 
     let traceData = [trace];
 
     let layout = {
-        title: "Top 10 Countries by Human Development Index (HDI)"
+        title: `Human Development Index (HDI) of the Countries in ${region}`
     };
     
-    Plotly.newPlot("bar", traceData, layout);
-
+    Plotly.newPlot("scatter", traceData, layout);
 }
 
 // Function to draw the radar chart
@@ -154,10 +148,94 @@ function drawRadarChart(region, data_js) {
     let inputRegion = data_js.filter(country => country.region === region);
 
     // Sort the inputSample by sample_values in the descending order
-    inputRegion.sort((a, b) => b["Human Development Index (HDI)"] - a["Human Development Index (HDI)"]);
+    inputRegion.sort((a, b) => b["Human Development Index (HDI)"].localeCompare(a["Human Development Index (HDI)"]));
+    let top_country = inputRegion[0];
+    let second_top_country = inputRegion[1];
+    let thrid_top_country = inputRegion[2];
+    let fourth_top_country = inputRegion[3];
+    let fifth_top_country = inputRegion[4];
+
+    let marksCanvas = document.getElementById("radar");
+
+    // Check if there's an existing chart
+    let chartExists = Chart.getChart(marksCanvas);
+
+    // If a chart exists, destroy it
+    if (chartExists) {
+        chartExists.destroy();
+    }
     
-    top_country = inputRegion[0];
-    
-    
+    Chart.defaults.font.family = "Lato";
+    Chart.defaults.font.size = 22;
+    Chart.defaults.color = "black";
+
+    let marksData = {
+    labels: ["LPP Index", "Crime Rate", "Healthcare Index"],
+    datasets: [
+        {
+            label: top_country.Countries,
+            backgroundColor: "#FFF17644",
+            borderColor: "black",
+            borderWidth: 1,
+            data: [top_country.LPP_index, top_country.crime_index, top_country.healthcare_index]
+        },
+        {
+            label: second_top_country.Countries,
+            backgroundColor: "#FFA50044",
+            borderColor: "black",
+            borderWidth: 1,
+            data: [second_top_country.LPP_index, second_top_country.crime_index, second_top_country.healthcare_index]
+        },
+        {
+            label: thrid_top_country.Countries,
+            backgroundColor: "#00FF0044",
+            borderColor: "black",
+            borderWidth: 1,
+            data: [thrid_top_country.LPP_index, thrid_top_country.crime_index, thrid_top_country.healthcare_index]
+        },
+        {
+            label: fourth_top_country.Countries,
+            backgroundColor: "#0000FF44",
+            borderColor: "black",
+            borderWidth: 1,
+            data: [fourth_top_country.LPP_index, fourth_top_country.crime_index, fourth_top_country.healthcare_index]
+        },
+        {
+            label: fifth_top_country.Countries,
+            backgroundColor: "#80008044",
+            borderColor: "black",
+            borderWidth: 1,
+            data: [fifth_top_country.LPP_index, fifth_top_country.crime_index, fifth_top_country.healthcare_index]
+        }
+    ]
+    };
+
+    let chartOptions = {
+    plugins: {
+        title: {
+        display: true,
+        align: "center",
+        text: "Data for the Top 5 Countries with the Highest HDI"
+        },
+        legend: {
+        align: "center"
+        }
+    },
+    scales: {
+        r: {
+        pointLabels: {
+            font: {
+            size: 20
+            }
+        }
+        }
+    }
+    };
+
+    new Chart(marksCanvas, {
+        type: "radar",
+        data: marksData,
+        options: chartOptions
+    });
 
 }
